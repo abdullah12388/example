5112ac3619a214330c4e2505293b191c9fc6b78f
Abdullah <abdullah.mk96@yahoo.com>
2023-03-04 12:34:09 +0200

    Update views.py

--- exam/zoo/views.py
+++ exam/zoo/views.py
@@ -1,58 +1,62 @@
 from django.http import HttpResponse, JsonResponse
-from django.views import generic
-from django.shortcuts import get_object_or_404
+from django.views import View
 from .models import Animal, Species
-from datetime import datetime, timedelta
 from django.utils import timezone
 
 
-class AnimalPopulationView(generic.View):
+class AnimalPopulationView(View):
     def get(self, request):
         animals_number = Animal.objects.all().count()
         return HttpResponse('Total Animals = ' + str(animals_number))
 
 
-class AnimalView(generic.View):
-    def get(self, request, name):
-        animal_object = get_object_or_404(Animal, name=name)
+class AnimalView(View):
+    def get(self, request):
+        animal_name = request.GET.get('name')
+        try:
+            animal_object = Animal.objects.get(name=animal_name)
+        except Animal.DoesNotExist:
+            return HttpResponse(status=404)
         returned_data = {
             "name": animal_object.name,
             "species": animal_object.species.name,
-            "last_feed_time": animal_object.last_feed_time
+            "last_feed_time": animal_object.last_feed_time.isoformat(),
         }
-        return JsonResponse(data=returned_data)
+        return JsonResponse(returned_data)
 
-    def post(self, request, format=None):
+    def post(self, request):
         animal_name = request.POST.get('name')
         animal_species = request.POST.get('species')
-
-        check_species = Species.objects.filter(name=animal_species)
-        if check_species.count() <= 0:
-            Species.objects.create(name=animal_species)
-        species_obj = Species.objects.get(name=animal_species)
-        animal_obj = Animal.objects.create(name=animal_name, species=species_obj)
-        if animal_obj:
+        if len(animal_name) > 30 or len(animal_species) > 30:
+            return HttpResponse(status=400)
+        try:
+            animal_obj = Animal.objects.get(name=animal_name)
+            return HttpResponse('Already Exists!!', status=422)
+        except:
+            species_obj, _ = Species.objects.get_or_create(name=animal_species)
+            animal_obj = Animal.objects.create(name=animal_name, species=species_obj)
             returned_data = {
                 "name": animal_obj.name,
                 "species": animal_obj.species.name,
-                "last_feed_time": animal_obj.last_feed_time
+                "last_feed_time": animal_obj.last_feed_time.isoformat(),
             }
             return JsonResponse(data=returned_data, status=201)
-        else:
-            return JsonResponse(status=422)
 
 
-class HungryAnimalsView(generic.View):
+class HungryAnimalsView(View):
     def get(self, request):
-        date_from_two_days = timezone.now() - timedelta(days=2)
+        date_from_two_days = timezone.now() - timezone.timedelta(days=2)
         animals_number = Animal.objects.filter(last_feed_time__lte=date_from_two_days)
         return HttpResponse('Animals hungry = ' + str(animals_number.count()))
 
 
-class FeedAnimalView(generic.View):
+class FeedAnimalView(View):
     def post(self, request):
         animal_name = request.POST.get('name')
-        animal_obj = Animal.objects.get(name=animal_name)
+        try:
+            animal_obj = Animal.objects.get(name=animal_name)
+        except Animal.DoesNotExist:
+            return HttpResponse(status=404)
         animal_obj.last_feed_time = timezone.now()
         animal_obj.save()
-        return HttpResponse('Updated Successfully')
+        return HttpResponse('Animal Fed Successfully')
